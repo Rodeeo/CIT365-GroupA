3fcea93ad4024c2b469edfca18901f3966038e2f
jessegj <jen18055@byui.edu>
2021-05-20 21:28:27 -0600

    Built Grid View

---
+++ MegaDesk-Yaona/DataGridView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MegaDesk_Yaona
+{
+    public class DataGridView
+    {
+        public string Customer { get; set; }
+        public int DateOrdered { get; set; }
+        public int Dimension { get; set; }
+        public int Material { get; set; }
+        public int Price { get; set; }
+
+    }
+}
