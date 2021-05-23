ee8b82d72cddbf86cd4d6e3f69b165a89cab1c01
oleayaona <oleayaona@gmail.com>
2021-05-22 20:19:00 -0700

    view quotes datagridview

--- MegaDesk-Yaona/ViewAllQuotes.Designer.cs
+++ MegaDesk-Yaona/ViewAllQuotes.Designer.cs
@@ -32,6 +32,13 @@ namespace MegaDesk_Yaona
             this.BackBtn = new System.Windows.Forms.Button();
             this.viewQuotesGrid = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.viewQuoteCustomerNameLabel = new System.Windows.Forms.Label();
+            this.viewQuoteCostLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.viewQuoteSpecsLabel = new System.Windows.Forms.Label();
+            this.viewQuoteMaterialLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.viewQuotesGrid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,12 +54,13 @@ namespace MegaDesk_Yaona
             // 
             // viewQuotesGrid
             // 
-            this.viewQuotesGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.viewQuotesGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.viewQuotesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.viewQuotesGrid.Location = new System.Drawing.Point(12, 90);
             this.viewQuotesGrid.Name = "viewQuotesGrid";
             this.viewQuotesGrid.RowTemplate.Height = 25;
-            this.viewQuotesGrid.Size = new System.Drawing.Size(540, 234);
+            this.viewQuotesGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.viewQuotesGrid.Size = new System.Drawing.Size(540, 132);
             this.viewQuotesGrid.TabIndex = 1;
             this.viewQuotesGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -66,11 +74,83 @@ namespace MegaDesk_Yaona
             this.label1.TabIndex = 2;
             this.label1.Text = "All Saved Quotes";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(13, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Quote Details:";
+            // 
+            // viewQuoteCustomerNameLabel
+            // 
+            this.viewQuoteCustomerNameLabel.AutoSize = true;
+            this.viewQuoteCustomerNameLabel.Location = new System.Drawing.Point(12, 264);
+            this.viewQuoteCustomerNameLabel.Name = "viewQuoteCustomerNameLabel";
+            this.viewQuoteCustomerNameLabel.Size = new System.Drawing.Size(38, 15);
+            this.viewQuoteCustomerNameLabel.TabIndex = 4;
+            this.viewQuoteCustomerNameLabel.Text = "label3";
+            // 
+            // viewQuoteCostLabel
+            // 
+            this.viewQuoteCostLabel.AutoSize = true;
+            this.viewQuoteCostLabel.Location = new System.Drawing.Point(12, 283);
+            this.viewQuoteCostLabel.Name = "viewQuoteCostLabel";
+            this.viewQuoteCostLabel.Size = new System.Drawing.Size(38, 15);
+            this.viewQuoteCostLabel.TabIndex = 5;
+            this.viewQuoteCostLabel.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(241, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 21);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Desk Details:";
+            // 
+            // viewQuoteSpecsLabel
+            // 
+            this.viewQuoteSpecsLabel.AutoSize = true;
+            this.viewQuoteSpecsLabel.Location = new System.Drawing.Point(241, 277);
+            this.viewQuoteSpecsLabel.Name = "viewQuoteSpecsLabel";
+            this.viewQuoteSpecsLabel.Size = new System.Drawing.Size(38, 15);
+            this.viewQuoteSpecsLabel.TabIndex = 7;
+            this.viewQuoteSpecsLabel.Text = "label6";
+            // 
+            // viewQuoteMaterialLabel
+            // 
+            this.viewQuoteMaterialLabel.AutoSize = true;
+            this.viewQuoteMaterialLabel.Location = new System.Drawing.Point(241, 296);
+            this.viewQuoteMaterialLabel.Name = "viewQuoteMaterialLabel";
+            this.viewQuoteMaterialLabel.Size = new System.Drawing.Size(38, 15);
+            this.viewQuoteMaterialLabel.TabIndex = 8;
+            this.viewQuoteMaterialLabel.Text = "label7";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(241, 315);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
             // ViewAllQuotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(564, 361);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.viewQuoteMaterialLabel);
+            this.Controls.Add(this.viewQuoteSpecsLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.viewQuoteCostLabel);
+            this.Controls.Add(this.viewQuoteCustomerNameLabel);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.viewQuotesGrid);
             this.Controls.Add(this.BackBtn);
@@ -89,5 +169,12 @@ namespace MegaDesk_Yaona
         private System.Windows.Forms.Button BackBtn;
         private System.Windows.Forms.DataGridView viewQuotesGrid;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label viewQuoteCustomerNameLabel;
+        private System.Windows.Forms.Label viewQuoteCostLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label viewQuoteSpecsLabel;
+        private System.Windows.Forms.Label viewQuoteMaterialLabel;
+        private System.Windows.Forms.Label label3;
     }
 }
