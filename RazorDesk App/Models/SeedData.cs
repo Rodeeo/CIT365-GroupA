feb9dffc1313ce47096315895b84d471a6719781
Rodeeo <therodee@gmail.com>
2021-06-10 17:22:09 -0600

    RazorDesk Push

---
+++ RazorDesk App/Models/SeedData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorDesk_App.Data;
+
+namespace RazorDesk_App.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorDesk_AppContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorDesk_AppContext>>()))
+            {
+                // Look for any movies.
+                if (context.DeskQuote.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.DeskQuote.AddRange(
+                    new DeskQuote
+                    {
+                        FirstName = "Harry",
+                        LastName = "Potter",
+                        Width = 25,
+                        Depth = 13,
+                        NumberOfDrawers = 3,
+                        DesktopMaterial = DesktopMaterial.Laminate,
+                        QuoteDate = DateTime.Parse("2021-06-01"),
+                        RushDays = 0,
+                    },
+                    new DeskQuote
+                    {
+                        FirstName = "Luke",
+                        LastName = "Skywalker ",
+                        Width = 35,
+                        Depth = 33,
+                        NumberOfDrawers = 5,
+                        DesktopMaterial = DesktopMaterial.Oak,
+                        QuoteDate = DateTime.Parse("2021-06-02"),
+                        RushDays = 3,
+                    },
+                    new DeskQuote
+                    {
+                        FirstName = "Frodo",
+                        LastName = "Bagins",
+                        Width = 45,
+                        Depth = 23,
+                        NumberOfDrawers = 3,
+                        DesktopMaterial = DesktopMaterial.Veneer,
+                        QuoteDate = DateTime.Parse("2021-06-03"),
+                        RushDays = 5,
+                    }, new DeskQuote
+                    {
+                        FirstName = "Jean Luc",
+                        LastName = "Pickard",
+                        Width = 65,
+                        Depth = 48,
+                        NumberOfDrawers = 3,
+                        DesktopMaterial = DesktopMaterial.Pine,
+                        QuoteDate = DateTime.Parse("2021-06-04"),
+                        RushDays = 7,
+                    }
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+    
---
+++ RazorDesk App/Pages/DeskQuotes/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorDesk_App.Data;
+using RazorDesk_App.Models;
+
+namespace RazorDesk_App.Pages.DeskQuotes
+{
+    public class EditModel : PageModel
+    {
+        private readonly RazorDesk_App.Data.RazorDesk_AppContext _context;
+
+        public EditModel(RazorDesk_App.Data.RazorDesk_AppContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public DeskQuote DeskQuote { get; set; }
+
+        public List<DesktopMaterial> MaterialList = Enum.GetValues(typeof(DesktopMaterial)).Cast<DesktopMaterial>().ToList();
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            DeskQuote = await _context.DeskQuote.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (DeskQuote == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(DeskQuote).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DeskQuoteExists(DeskQuote.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool DeskQuoteExists(int id)
+        {
+            return _context.DeskQuote.Any(e => e.ID == id);
+        }
+    }
+}
